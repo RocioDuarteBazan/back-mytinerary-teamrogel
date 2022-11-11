088ff719e585d6fa5e7eb01db5acbaeb8df8395c
Your Name01 <rocioduarte197@gmail.com>
2022-11-11 17:55:31 -0300

    mt-01-18

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
-const router = require('express').Router();
+let router = require('express').Router();
 const user = require ('./user')
+const city = require ('./city')
+
 
 router.use('/api/users', user )
+router.use('/api/cities', city )
 
 module.exports = router;
