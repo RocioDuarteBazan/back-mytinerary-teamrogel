5e0bf487a92dfa046cec384aaf0af6c0e6155ad9
Your Name01 <rocioduarte197@gmail.com>
2022-11-12 16:08:10 -0300

    mt-01-20

---
+++ controllers/itinerary.js
@@ -0,0 +1,24 @@
+const Itinerary = require('../models/Itinerary');
+
+const controller = {
+
+    create: async (req, res) => {
+        try {         
+            let new_itinerary = await Itinerary.create(req.body);
+            res.status(201).json(
+                {
+                id: new_itinerary._id,
+                success: true,
+                message: 'Itinerary created successfully'
+                }
+            )
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
+}
+
+module.exports = controller;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 let router = require('express').Router();
-const user = require ('./user')
-const city = require ('./city')
+const user = require ('./user');
+const city = require ('./city');
+const itinerary = require ('./itinerary')
 
 router.use('/api/users', user )
 router.use('/api/cities', city )
+router.use('/api/itineraries', itinerary )
 
 module.exports = router;
