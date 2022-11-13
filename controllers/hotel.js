55a6edc35cf13a325794a067d768426b38f3bc05
maurito.harretche <mauroharretche@gmail.com>
2022-11-13 15:04:52 -0300

    mt-02-26

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -23,7 +23,7 @@ const controller = {
         let query = {};
         let order = {};
         if (req.query.name) {
-            query = { name: { $regex: req.query.name, $options: 'i' }}
+            query = { name: { $regex: req.query.name, $options: 'i' } }
         }
         if (req.query.order) {
             order = { capacity: req.query.order }
@@ -38,6 +38,29 @@ const controller = {
                     message: 'Hotel read successfully'
                 }
             )
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
+    readOnlyOne: async (req, res) => {
+        let id = req.params.id;
+        try {
+            let hotel = await Hotel.findOne({ _id: id }).populate({ path: 'userId', select: 'name photo -_id' });
+            if (hotel) {
+                res.status(200).json({
+                    response: hotel,
+                    success: true,
+                    message: "Found hotel"
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "No hotel obtained"
+                })
+            }
         } catch (error) {
             res.status(400).json({
                 success: false,
---
+++ controllers/show.js
@@ -0,0 +1,51 @@
+const Show = require('../models/Show');
+
+const controller = {
+
+    create: async (req, res) => {
+        try {
+            let new_show = await Show.create(req.body);
+            res.status(201).json(
+                {
+                    id: new_show._id,
+                    success: true,
+                    message: 'Show created successfully'
+                }
+            )
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
+    readShows: async (req, res) => {
+        let query = {}
+
+        if (req.query.hotelId) {
+            query = { hotelId: req.query.hotelId };
+        }
+        try {
+            let shows = await Show.find(query);
+            if (shows) {
+                res.status(200).json({
+                    success: true,
+                    message: "Show was found",
+                    data: shows,
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "Show not found"
+                })
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
+}
+
+module.exports = controller;
