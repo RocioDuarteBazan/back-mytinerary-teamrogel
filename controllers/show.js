2ff8bcd8f0a85a71088b062be6259cb73fb4fac7
maurito.harretche <mauroharretche@gmail.com>
2022-11-14 14:47:14 -0300

    mt-b2-34

--- controllers/show.js
+++ controllers/show.js
@@ -46,6 +46,28 @@ const controller = {
             })
         }
     },
+    update: async (req, res) => {
+        let { id } = req.params
+        try {
+            let show = await Show.findOneAndUpdate({ _id: id }, req.body, { new: true })
+            if (show) {
+                res.status(200).json({
+                    success: true,
+                    message: "The show was successfully modified"
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "There is no show that matches"
+                })
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
 }
 
 module.exports = controller;
