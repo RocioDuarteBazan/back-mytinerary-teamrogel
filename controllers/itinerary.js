c9588fe70c6f9751f1630ec504c6be94facae723
Your Name01 <rocioduarte197@gmail.com>
2022-11-13 22:04:01 -0300

    mt-b1-31

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -1,24 +1,6 @@
 const Itinerary = require('../models/Itinerary');
 
 const controller = {
-
-    create: async (req, res) => {
-        try {
-            let new_itinerary = await Itinerary.create(req.body);
-            res.status(201).json(
-                {
-                    id: new_itinerary._id,
-                    success: true,
-                    message: 'Itinerary created successfully'
-                }
-            )
-        } catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message
-            })
-        }
-    },
     readItineraries: async (req, res) => {
         let query = {}
 
@@ -39,6 +21,23 @@ const controller = {
                     message: "Event not found"
                 })
             }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
+    create: async (req, res) => {
+        try {
+            let new_itinerary = await Itinerary.create(req.body);
+            res.status(201).json(
+                {
+                    id: new_itinerary._id,
+                    success: true,
+                    message: 'Itinerary created successfully'
+                }
+            )
         } catch (error) {
             res.status(400).json({
                 success: false,
