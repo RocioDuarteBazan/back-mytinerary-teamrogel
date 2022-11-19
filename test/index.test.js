cd90b182100aeb378434b4cf49f9056bbe811f96
Your Name01 <rocioduarte197@gmail.com>
2022-11-19 09:57:45 -0300

    fix-mt-01-37

--- test/index.test.js
+++ test/index.test.js
@@ -4,19 +4,22 @@ const assert = chai.assert
 const request = require('supertest')
 
 
-describe('GET /api/cities', function(){
+describe('GET /api/cities', function () {
 
-    it('verify that it is an array of cities', function(done){
+    it('verify that it is an array of cities', function (done) {
         request(app)
-        .get('/api/cities/')
-        .expect(response => {
-            assert.typeOf(response.body.data, 'array', 'Is Array')
-        })
-        .end(function(err, res){
-            if(err){
-                return done(err)
-            }
-            done()
-        })
+            .get('/api/cities/')
+            .expect(res => {
+                assert.isArray(res.body.data)
+                res.body.data.forEach(city => {
+                    assert.isObject(city)
+                })
+            })
+            .end(function (err, res) {
+                if (err) {
+                    return done(err)
+                }
+                done()
+            })
     })
 })
