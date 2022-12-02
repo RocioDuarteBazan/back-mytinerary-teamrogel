81d0982f60840e2310907d3e9bc537ce7ac77dd2
Your Name01 <rocioduarte197@gmail.com>
2022-12-02 05:05:02 -0300

    fix-mt-01-55

--- models/Reaction.js
+++ models/Reaction.js
@@ -5,7 +5,7 @@ const schema = new mongoose.Schema({
     name: { type: String, required: true },
     icon: {type: String, required: true},
     iconBack: {type: String, required: true},
-    userId: {type: mongoose.Types.ObjectId, ref: 'users', required: true},
+    userId: [{type: mongoose.Types.ObjectId, ref: 'users', required: true}],
 });
 
 const Reaction = mongoose.model ("reactions", schema);
